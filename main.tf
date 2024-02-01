f74ed4111bf56b2871bee4bf1ec7b6944ba7d8f7
CodeOpsDynamics <157052451+CodeOpsDynamics@users.noreply.github.com>
2024-02-01 18:39:23 +0530

    local

---
+++ main.tf
@@ -0,0 +1,15 @@
+locals {
+  rg  = "rg1"
+  rge = "1strg"
+  tags = {
+    environment = "terraform demo"
+    managed_by  = "terraform"
+  }
+}
+
+
+resource "azurerm_resource_group" "rgconnect" {
+  for_each = var.rg
+  name     = "${local.rg}${each.value.name}${local.rge}"
+  location = "West Europe"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,11 @@
+rg = {
+  "1" = {
+    name = "resource-group1"
+  }
+  "2" = {
+    name = "resource-group2"
+  }
+  "3" = {
+    name = "resource-group3"
+  }
+}
